39c3b74bb94ac65b70e647e52248e2368101214b
AnnaxGrace <anna.grace.conover@gmail.com>
2020-10-05 14:05:58 -0500

    Added parent class and method

---
+++ Language.java
@@ -0,0 +1,23 @@
+public class Language {
+
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+
+  public Language(String langName, int speakers, String regions, String word){
+    name = langName;
+    numSpeakers = speakers;
+    regionsSpoken = regions;
+    wordOrder = word;
+
+  }
+
+  public void getInfo() {
+    System.out.println(name + " is spoken by " + numSpeakers + " people mainly in " + regionsSpoken + ". The language follows the word order: " + wordOrder);
+  }
+  public static void main(String[] args) {
+    Language Spanish = new Language("Spanish", 2, "many", "subject-verb-object");
+    Spanish.getInfo();
+  }
+}
